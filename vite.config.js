cdb158cdf35d4a72c4c2a8a64cdad2d3d7a97162
unknown <jereregalzi93@gmail.com>
2025-11-12 12:17:37 -0300

    fix success and redirect

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-export default defineConfig({ plugins: [react()] })
+import { viteStaticCopy } from 'vite-plugin-static-copy'
+
+export default defineConfig({
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        { src: 'public/_redirects', dest: '.' },
+        { src: 'public/success.html', dest: '.' },
+      ],
+    }),
+  ],
+})
